67f0ce9b84db556156e71342cb0350f2cadd1efa
Hayden Turek <haydenturek@gmail.com>
2017-08-19 02:39:01 -0500

    error handling

--- app.js
+++ app.js
@@ -2,13 +2,10 @@
 const env = process.env.NODE_ENV || 'development';
 const express = require('express');
 const path = require('path');
-// const morgan = require('morgan');
 const request = require('request');
 const bodyParser = require('body-parser');
 const rp = require('request-promise');
 const apiKey = 'AIzaSyCAiwNeQvlHKtKOc7_QJ0mfEgVaUgo5Eu8';
-// const apiKey = 'AIzaSyB87efOaaHv_mu2guTvd7CdrvWixFd1ooA';
-// const apiKey = 'AIzaSyAt57aFTIxEQWj8DZyWSbWBlQ4KHHSXsk8';
 
 const app = express();
 
@@ -18,41 +15,54 @@ if (env === 'development') {
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res, next) => {
-  res.send('root');
-});
-
 app.post('/places', (req, res, next) => {
-  const promises = [];
+
   const result = [];
-  let url;
+  const promises = [];
+  let url,
+      parsed;
+
   req.body.places.forEach(placeId => {
-    url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`;
+    url = 'https://maps.googleapis.com/maps/api/place/details/json?' +
+          `placeid=${placeId}&key=${apiKey}`;
     promises.push(rp(url));
   });
+
   Promise.all(promises).then(data => {
     data.forEach(datum => {
-      let parsed = JSON.parse(datum);
-      if (parsed.result.website) {
-        result.push(parsed.result.website);
-      } else  {
-        result.push(null);
+      parsed = JSON.parse(datum);
+      if (parsed.error) {
+        const error = new Error(parsed.error.message);
+        error.status = parsed.error.code;
+        next(error);
+      }
+      if (parsed.result) {
+        if (parsed.result.website) {
+          result.push(parsed.result.website);
+        } else  {
+          result.push(null);
+        }
+        res.json({result: result});
       }
     });
-    console.log(result);
-    res.json({result: result});
   }).catch(error => {
-    console.error(error);
-    res.json({error: error});
+    res.json({message: error});
   });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({message: err.message});
 });
 
 module.exports = app;
